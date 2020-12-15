17e32bbfe1227890f052d7802c97988d22fb236a
Muhammad Abel Jollando <72371489+AbelJollando@users.noreply.github.com>
2020-12-15 13:32:06 +0700

    Update TUBES.c

--- TUBES.c
+++ TUBES.c
@@ -113,10 +113,97 @@ void tukar_poin (int poin){
             printf("Point anda sudah habis\n");
         }
 }
+typedef struct{//Perlu dirubah bila ingin di update
+	char Nama[50];
+	char Member[20];
+	char Telp[13];
+	long ID;
+}Nota;
+FILE*PENJUALAN;
+char txt[20] = "Nota Penjualan.txt";
+
+void Create(){
+	Nota Penjualan;
+	PENJUALAN=fopen(txt,"a");
+	printf("Masukkan Nama Customer \t: ");scanf("%[^\n]",&Penjualan.Nama);fflush(stdin);
+	printf("Masukkan ID Customer \t: ");scanf("%i",&Penjualan.ID);fflush(stdin);
+	printf("Masukkan Member Customer \t: ");scanf("%s",&Penjualan.Member);fflush(stdin);
+	printf("Masukkan No Telepon Customer \t: ");scanf("%[^\n]",&Penjualan.Telp);fflush(stdin);
+	fprintf(PENJUALAN,"%s_%i_%s_%s\n",Penjualan.Nama,Penjualan.ID,Penjualan.Member,Penjualan.Telp);
+	fclose(PENJUALAN);
+}
+
+void Delete(){
+	Nota Penjualan[50];
+	int id,index,i;
+	printf("Masukan ID Customer yang ingin dihapus \t: ");scanf("%i",&id);fflush(stdin);
+	PENJUALAN=fopen("Nota Penjualan.txt","r");
+	index=0;
+	while (!feof(PENJUALAN)){
+		fscanf(PENJUALAN,"%[^_]_%i_%s_%[^\n]\n",&Penjualan[index].Nama,&Penjualan[index].ID,&Penjualan[index].Member,&Penjualan[index].Telp);
+		fflush(stdin);
+		if(id!=Penjualan[index].ID){
+			index++;
+		}
+	}
+	fclose(PENJUALAN);
+	PENJUALAN=fopen("Nota Penjualan.txt","w");
+	fclose(PENJUALAN);
+	PENJUALAN=fopen("Nota Penjualan.txt","a");
+	for(i=0;i<index;i++){
+		fprintf(PENJUALAN,"%s_%i_%s_%s\n",Penjualan[i].Nama,Penjualan[i].ID,Penjualan[i].Member,Penjualan[i].Telp);
+	}
+	fclose(PENJUALAN);
+}
+
+void Update(){
+	Nota Penjualan[50];
+	int id,index,i;
+	printf("Masukan ID yang ingin diubah \t: ");scanf("%i",&id);fflush(stdin);
+	PENJUALAN=fopen("Nota Penjualan.txt","r");
+	index=0;
+	while (!feof(PENJUALAN)){
+		fscanf(PENJUALAN,"%[^_]_%i_%s_%[^\n]\n",&Penjualan[index].Nama,&Penjualan[index].ID,&Penjualan[index].Member,&Penjualan[index].Telp);
+		fflush(stdin);
+		if(id==Penjualan[index].ID){
+			printf("Masukkan Nama Customer \t: ");scanf("%[^\n]",&Penjualan[index].Nama);fflush(stdin);
+			printf("Masukkan ID Customer \t: ");scanf("%i",&Penjualan[index].ID);fflush(stdin);
+			printf("Masukkan Member Customer :\t");scanf("%s",&Penjualan[index].Member);fflush(stdin);
+			printf("Masukkan Telp Customer\t: ");scanf("%[^\n]",&Penjualan[index].Telp);fflush(stdin);
+		}
+		index++;
+	}
+	fclose(PENJUALAN);
+	PENJUALAN=fopen("Nota Penjualan.txt","w");
+	fclose(PENJUALAN);
+	PENJUALAN=fopen("Nota Penjualan.txt","a");
+	for(i=0;i<index;i++){
+		fprintf(PENJUALAN,"%s_%i_%s_%s\n",Penjualan[i].Nama,Penjualan[i].ID,Penjualan[i].Member,Penjualan[i].Telp);
+	}
+	fclose(PENJUALAN);
+}
+
+void Read(){
+	Nota Penjualan;
+	int id;
+	printf("Masukan ID yang ingin ditampilkan \t: ");scanf("%i",&id);fflush(stdin);
+	PENJUALAN=fopen("Nota Penjualan.txt","r");
+	while (!feof(PENJUALAN)){
+		fscanf(PENJUALAN,"%[^_]_%i_%s_%s_%[^\n]\n",&Penjualan.Nama,&Penjualan.ID,&Penjualan.Member,&Penjualan.Telp);
+		fflush(stdin);
+		if(id==Penjualan.ID){
+			printf("Nama Customer \t : %s\n",Penjualan.Nama);
+			printf("ID Customer \t : %i\n",Penjualan.ID);
+			printf("Member Customer \t : %s\n",Penjualan.Member);
+			printf("No Telepon Customer \t : %s\n\n",Penjualan.Telp);
+		}
+	}
+}
 
 int main(){
 	
-	int pilihan1,point;
+	int pilihan1,point,menu;
+	char Lanjut[2];
 	TotalBelanja();
     point = hitung_poin(total);
 	Diskon();
@@ -128,6 +215,26 @@ int main(){
         
         tukar_poin(point);
     }
+	do{
+		printf("====================Menu====================\n");
+		printf("1. Create\n2. Read\n3. Update\n4. Delete\n===========================================\nPilihan >> ");
+		scanf("%i",&menu);fflush(stdin);
+		switch(menu){
+			case 1:
+				Create();
+			break;
+			case 2:
+				Read();
+			break;
+			case 3:
+				Update();
+			break;
+			case 4:
+				Delete();
+			break;
+		}
+		printf("Apakah anda ingin melanjutkan? (y/t): "); scanf("%[^\n]",&Lanjut);fflush(stdin);
+	}while(strcmp(Lanjut,"y")==0);
 }
 
 
